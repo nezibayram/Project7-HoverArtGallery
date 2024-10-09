aa41985f7bce77b3fd9abb40ce6093fa5be8ab93
Nezihe Bayram <nezihe.bayram22@gmail.com>
2024-10-09 21:34:59 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+function showProperties(element){
+    document.getElementById('message').innerHTML = element.alt;
+}
+
+function background1() {
+    document.querySelector('.outputBox').style.background = "url('images/image1.jpg')"
+}
+
+function background2() {
+    document.querySelector('.outputBox').style.background = "url('images/image2.jpg')"
+}
+
+function background3() {
+    document.querySelector('.outputBox').style.background = "url('images/image3.jpg')"
+}
+
+
